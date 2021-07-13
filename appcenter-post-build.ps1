c522a7f5451b3f8f1f318e61eaaa2fd5a1877bf0
Lukas Cenovsky <lucen@microsoft.com>
2021-07-13 12:47:42 +0200

    post build

--- appcenter-post-build.ps1
+++ appcenter-post-build.ps1
@@ -1,4 +1,5 @@
 Write-Host $env:APPCENTER_BUILD_ID
 $env:APPCENTER_BUILD_ID = "22"
 Write-Host "##vso[task.setvariable variable=APPCENTER_BUILD_ID]23"
+Write-Host "##vso[build.updatebuildnumber]76"
 Write-Host $env:APPCENTER_BUILD_ID
