6439755d6671d7110bb594c4d3a887ac51a5d281
Lukas Cenovsky <lucen@microsoft.com>
2021-07-13 12:05:41 +0200

    build number

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,4 +1,5 @@
 Write-Host $env:APPCENTER_BUILD_ID
 $env:APPCENTER_BUILD_ID = "32"
 Write-Host "##vso[task.setvariable variable=APPCENTER_BUILD_ID]33"
+Write-Host "##vso[build.updatebuildnumber]66"
 Write-Host $env:APPCENTER_BUILD_ID
