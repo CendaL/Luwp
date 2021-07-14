cd0d15fcde74acc838b1e50ac8421067243e45e0
Lukas Cenovsky <lucen@microsoft.com>
2021-07-14 17:10:29 +0200

    fix version

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,3 +1,5 @@
 Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
 Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
-Write-Host "##vso[build.updatebuildnumber]$($env:BUILD_BUILDNUMBER + 10)"
+Write-Host "##vso[build.updatebuildnumber]$([int]$env:BUILD_BUILDNUMBER + 10)"
+Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
+Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
