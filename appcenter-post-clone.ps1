e13a11ff366a785c5740d2f9718120f8689ca51a
Lukas Cenovsky <lucen@microsoft.com>
2021-07-14 17:19:14 +0200

    sleep

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,5 +1,6 @@
 Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
 Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
 Write-Host "##vso[build.updatebuildnumber]$([int]$env:BUILD_BUILDNUMBER + 10)"
+Start-Sleep -Seconds 10
 Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
 Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
