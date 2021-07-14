eaaa1bb9bac738997bf8365b97fb9f5886340031
Lukas Cenovsky <lucen@microsoft.com>
2021-07-14 16:39:37 +0200

    version test

--- appcenter-post-build.ps1
+++ appcenter-post-build.ps1
@@ -1,5 +1,2 @@
-Write-Host $env:APPCENTER_BUILD_ID
-$env:APPCENTER_BUILD_ID = "22b"
-Write-Host "##vso[task.setvariable variable=APPCENTER_BUILD_ID]23"
-Write-Host "##vso[build.updatebuildnumber]auto76b"
-Write-Host $env:APPCENTER_BUILD_ID
+Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
+Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,5 +1,3 @@
-Write-Host $env:APPCENTER_BUILD_ID
-$env:APPCENTER_BUILD_ID = "32a"
-Write-Host "##vso[task.setvariable variable=APPCENTER_BUILD_ID]33"
-Write-Host "##vso[build.updatebuildnumber]auto66"
-Write-Host $env:APPCENTER_BUILD_ID
+Write-Host "APPCENTER_BUILD_ID: $env:APPCENTER_BUILD_ID"
+Write-Host "BUILD_BUILDNUMBER:  $env:BUILD_BUILDNUMBER"
+Write-Host "##vso[build.updatebuildnumber]$($env:BUILD_BUILDNUMBER + 10)"
